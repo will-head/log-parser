241ae9cea44ebb8ef1b288aa501e8516fcd782e1
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-06 16:49:18 +0100

    Adds failing feature test

--- spec/features/log_parser_spec.rb
+++ spec/features/log_parser_spec.rb
@@ -1,6 +1,12 @@
-describe 'Log Parser' do
+describe 'parser.rb' do
   it 'can list webpages ordered by page views' do
-
+    expect { system %(./parser.rb ./data/feature_test.log) }.to output(<<~LIST_OF_WEBPAGES
+      /home 8 visits
+      /about/2 5 visits
+      /contact 5 visits
+      /about 2 visits
+    LIST_OF_WEBPAGES
+    ).to_stdout_from_any_process
   end
 
   it 'can list webpages ordered by uniqe page views' do
