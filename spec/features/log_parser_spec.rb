d3a12388d35800d3b2c7a6ee82b970d57dafc648
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-06 16:54:13 +0100

    Adds failing test for unique page views

--- spec/features/log_parser_spec.rb
+++ spec/features/log_parser_spec.rb
@@ -10,6 +10,12 @@ describe 'parser.rb' do
   end
 
   it 'can list webpages ordered by uniqe page views' do
-
+    expect { system %(./parser.rb --unique ./data/feature_test.log) }.to output(<<~LIST_OF_WEBPAGES
+      /home 6 unique views
+      /contact 4 unique views
+      /about 2 unique views
+      /about/2 2 unique views
+    LIST_OF_WEBPAGES
+    ).to_stdout_from_any_process
   end  
 end
