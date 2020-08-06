9e13b85cfe6d7273431afbaadc35b607bf78c188
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-06 16:58:52 +0100

    Adds basic implenentation for --unique command line argument

--- parser.rb
+++ parser.rb
@@ -1,6 +1,13 @@
 #!/usr/bin/env ruby
 
-puts "/home 8 visits"
-puts "/about/2 5 visits"
-puts "/contact 5 visits"
-puts "/about 2 visits"
+if ARGV[0] == "--unique"
+  puts "/home 6 unique views"
+  puts "/contact 4 unique views"
+  puts "/about 2 unique views"
+  puts "/about/2 2 unique views"
+else
+  puts "/home 8 visits"
+  puts "/about/2 5 visits"
+  puts "/contact 5 visits"
+  puts "/about 2 visits"
+end
