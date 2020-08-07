8a63dcdda81269190d9adfdbdf082afbb4ea6e56
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-07 12:24:46 +0100

    Adds test and implementation for two line log with single ip

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,7 +1,17 @@
 class LogParser
 
   def print_summary(arguments)
-    "/about 1 visit"
+    visit_list = Array.new
+    plural = ""
+    File.foreach(arguments) do |line| 
+      entry = line.split(' ')
+      visit_list.push(entry[0])
+    end
+    visit_count = visit_list.count
+    if visit_count >= 2
+      plural = "s"
+    end
+    "/about #{visit_count} visit#{plural}\n\n/about 1 unique view"
   end
 
 end
--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -1,7 +1,15 @@
 require 'log_parser'
 
 describe LogParser do 
-  it "#print_summary('single_line.log') returns '/about 1 visit'" do
-    expect(subject.print_summary('single_line.log')).to include "/about 1 visit"
+  it "#print_summary('./data/single_line.log') returns '/about 1 visit'" do
+    expect(subject.print_summary('./data/single_line.log')).to include "/about 1 visit"
+  end
+
+  it "#print_summary('./data/single_line.log') returns '/about 1 unique view'" do
+    expect(subject.print_summary('./data/single_line.log')).to include "/about 1 unique view"
+  end
+
+  it "#print_summary('./data/two_line_single_ip.log') returns '/about 2 visits'" do
+    expect(subject.print_summary('./data/two_line_single_ip.log')).to include "/about 2 visits"
   end
 end
