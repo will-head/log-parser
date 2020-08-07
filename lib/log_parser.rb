d47b02c5a3c8451a12b2fa9c9349b5641d601b17
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-07 14:10:47 +0100

    Adds test and implementation for two paths, one ip address

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -2,26 +2,32 @@ class LogParser
 
   def print_summary(arguments)
     # plural = ""
-    visit_count = process_log(arguments)
+    visit_list = process_log(arguments)
+    format_summary(visit_list)
     # if visit_count >= 2
     #   plural = "s"
     # end
-
-    # "/about #{visit_count} visit#{plural}\n /
-    "/about #{visit_count} visit\n /
-    \n /
-    /about 1 unique view"
   end
 
   private
 
   def process_log(file)
     visit_list = Array.new
+
     File.foreach(file) do |line| 
       entry = line.split(' ')
       visit_list.push(entry[0])
     end
-    visit_list.count
+    visit_list
+  end
+
+  def format_summary(visit_list)
+    visit_count = Hash.new(0)
+    visit_list.each { |entry| visit_count[entry] += 1 }
+    visit_summary = ""
+    visit_count.each_pair { |path, visits| visit_summary += "#{path} #{visits} visit\n" }
+    unique_summary = "/about 1 unique view\n"
+    visit_summary + "\n" + unique_summary
   end
 
 end
--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -9,9 +9,9 @@ describe LogParser do
     expect(subject.print_summary('./data/test_log.log')).to include "/about 1 unique view"
   end
 
-  # it "#print_summary('./data/test_log.log') includes '/about 2 visits'" do
-  #   expect(subject.print_summary('./data/test_log.log')).to include "/about 2 visits"
-  # end
+  it "#print_summary('./data/test_log.log') includes '/home 1 visit'" do
+    expect(subject.print_summary('./data/test_log.log')).to include "/home 1 visit"
+  end
 
   # it "#print_summary('./data/test_log.log') includes '/home 1 visit'" do
   #   expect(subject.print_summary('./data/test_log.log')).to include "/home 1 visit"
