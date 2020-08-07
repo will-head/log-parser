471142830686a8684fbd2a0b6b0834afe0cc3c38
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-07 16:19:45 +0100

    Adds test and implementation for unique views

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,29 +1,41 @@
 class LogParser
 
   def print_summary(arguments)
+    log = process_log(arguments)
 
-    visit_list = process_log(arguments)
-    format_summary(visit_list)
-
+    # visit_list = Array.new
+    # log.each do |line|
+    #   visit_list.push(line.keys[0].to_s)
+    # end
+    # format_summary(visit_list)
+    format_summary(log)
   end
 
   private
 
   def process_log(file)
-    visit_list = Array.new
+    log = Array.new
 
     File.foreach(file) do |line| 
       entry = line.split(' ')
-      visit_list.push(entry[0])
+      log.push({ entry[0] => entry[1] })
     end
-    visit_list
+    log
   end
 
-  def format_summary(visit_list)
+  def format_summary(log)
+
+    visit_list = Array.new
+    log.each do |line|
+      visit_list.push(line.keys[0].to_s)
+    end
+    # p visit_list
     visit_count = Hash.new(0)
     visit_list.each { |entry| visit_count[entry] += 1 }
     visit_summary = format_visit_summary(visit_count)
-    unique_summary = "/home 1 unique view\n"
+
+    unique_summary = format_unique_summary(log)
+
     visit_summary + "\n" + unique_summary
   end
 
@@ -38,6 +50,28 @@ class LogParser
 
   def format_visit_line(path, visits)
     "#{path} #{visits} visit#{format_plural(visits)}\n"
+  end
+
+  def format_unique_summary(log)
+    log.uniq!
+    # p log
+
+    unique_list = Array.new
+    log.each do |line|
+      unique_list.push(line.keys[0].to_s)
+    end
+    # p unique_list
+    unique_count = Hash.new(0)
+    unique_list.each { |entry| unique_count[entry] += 1 }
+    # p unique_count
+    unique_summary = ""
+    unique_count = unique_count.sort_by { |path, visits| [-visits, path] }.to_h
+    # p unique_count
+    unique_count.each_pair do |path, visits| 
+      unique_summary += "#{path} #{visits} unique view#{format_plural(visits)}\n"
+    end
+    # "/home 1 unique view\n"
+    unique_summary
   end
 
   def format_plural(amount)
--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -24,4 +24,16 @@ describe LogParser do
   it "#print_summary('./data/test_log.log') includes '/contact 2 visits\n/about 1 visit'" do
     expect(subject.print_summary('./data/test_log.log')).to include "/contact 2 visits\n/about 1 visit"
   end
+
+  it "#print_summary('./data/test_log.log') includes '/about 1 unique view'" do
+    expect(subject.print_summary('./data/test_log.log')).to include "/about 1 unique view"
+  end
+
+  it "#print_summary('./data/test_log.log') includes '/contact 1 unique view'" do
+    expect(subject.print_summary('./data/test_log.log')).to include "/contact 1 unique view"
+  end
+
+  it "#print_summary('./data/test_log.log') includes '/people 2 unique views'" do
+    expect(subject.print_summary('./data/test_log.log')).to include "/people 2 unique views"
+  end
 end
