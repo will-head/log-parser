4d868cef9431a931f61cddad0cb2e33163e09f9a
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-07 11:49:15 +0100

    Clarifies expected result

--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -1,7 +1,7 @@
 require 'log_parser'
 
 describe LogParser do 
-  it "#print_summary('single_line.log') returns expected output" do
+  it "#print_summary('single_line.log') returns '/about 1 visit'" do
     expect(subject.print_summary('single_line.log')).to include "/about 1 visit"
   end
 end
