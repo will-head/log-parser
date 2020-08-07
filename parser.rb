9964a97de6d4bbbb727c339b7589a820b377a4bd
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-07 21:24:40 +0100

    Refactors parser.rb to handle errors

--- parser.rb
+++ parser.rb
@@ -2,5 +2,9 @@
 
 require_relative "lib/log_parser"
 
-parser = LogParser.new
-puts parser.summary(ARGV[0])
+begin
+  parser = LogParser.new
+  puts parser.summary(ARGV[0])
+rescue StandardError => e
+  abort(e.to_s)
+end
