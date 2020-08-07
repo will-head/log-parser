ae3a25843f34b2578dea9729041618830ac845f4
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-07 16:35:04 +0100

    Adds LogParser instance to parser.rb

--- parser.rb
+++ parser.rb
@@ -1,11 +1,6 @@
 #!/usr/bin/env ruby
 
-puts "/home 9 visits"
-puts "/about/2 5 visits"
-puts "/contact 5 visits"
-puts "/about 1 visit"
-puts ""
-puts "/home 6 unique views"
-puts "/contact 5 unique views"
-puts "/about/2 2 unique views"
-puts "/about 1 unique view"
+require_relative "lib/log_parser"
+
+parser = LogParser.new
+puts parser.print_summary(ARGV[0])
